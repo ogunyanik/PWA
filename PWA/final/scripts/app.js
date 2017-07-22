54a353bcd079d001a372cfe1f5d9d240408fceff
Ogün Yanık <oyanik@tmobtech.com>
2017-07-23 01:58:22 +0300

    inital commit

--- PWA/final/scripts/app.js
+++ PWA/final/scripts/app.js
@@ -55,7 +55,7 @@
           } else {
               // Return the initial weather forecast since no data is available.
               app.updateForecastCard(initialWeatherForecast);
-          }
+             }
       };
       request.open('GET', url);
       request.send();
